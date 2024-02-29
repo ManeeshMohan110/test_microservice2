64b2e3e5bbc19a15b9fc36b8c44668b1eab4b75d
ideology <maneeshtmala@gmail.com>
2024-03-01 00:41:51 +0530

    Initial commit

---
+++ microservice2/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+admin.site.site_header='Microservice2-Admin'
+admin.site.site_header = "Microservice2-Admin'"
+admin.site.site_title = "MS2-Admin"
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+]
